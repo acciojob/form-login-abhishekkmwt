e53e3f4e907fcdb6a64c4dc8d2eb8c9ee2f0bcc4
Abhishek Kumawat <65283365+abhishekkmwt@users.noreply.github.com>
2023-06-26 18:57:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+    const form = document.getElementsByTagName("form")[0];
+    let firstName =form["fname"].value.trim();
+	let lastName =form["lname"].value.trim();
 
+    alert(firstName +" "+ lastName);
+	
+	
 }
